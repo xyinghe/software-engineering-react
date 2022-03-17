12f66798345e420f6eb70629a81a40f182bffc92
xyinghe <he.xuey@northeastern.edu>
2022-03-17 17:26:25 -0400

    fix URL typo

--- src/services/tuits-service.js
+++ src/services/tuits-service.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const TUITS_API = "https://cs5500-node-xyinghe.herokuapp.com//api/tuits";
+const TUITS_API = "https://cs5500-node-xyinghe.herokuapp.com/api/tuits";
 const USERS_API = "https://cs5500-node-xyinghe.herokuapp.com/api/users";
 // const TUITS_API = "http://localhost:4000/api/tuits";
 // const USERS_API = "http://localhost:4000/api/users";
