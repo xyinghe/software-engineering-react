91f142c9420a5a42f14c39f88e83d98deeb1fc12
xyinghe <he.xuey@northeastern.edu>
2022-03-28 16:22:28 -0400

    adding dislike button works

--- src/components/tuits/index.js
+++ src/components/tuits/index.js
@@ -4,16 +4,22 @@ import Tuit from "./tuit";
 import * as likesService from "../../services/likes-service";
 import * as service from "../../services/tuits-service";
 import * as authService from "../../services/auth-service";
+import * as dislikesService from "../../services/dislike-service";
 const Tuits = ({tuits = [], refreshTuits}) => {
 
 
     const likeTuit = (tuit) =>
-        likesService.userLikesTuit("me", tuit._id)
+        likesService.userTogglesTuitLikes("me", tuit._id)
             .then(refreshTuits)
             .catch(e => alert(e))
-        const deleteTuit = (tid) =>
-            service.deleteTuit(tid)
-                .then(refreshTuits);
+
+    const dislikeTuit = (tuit) =>
+        dislikesService.userTogglesTuitDislikes("me", tuit._id)
+            .then(refreshTuits)
+            .catch(e => alert(e))
+    const deleteTuit = (tid) =>
+        service.deleteTuit(tid)
+            .then(refreshTuits);
 
         return (
             <div>
@@ -23,6 +29,7 @@ const Tuits = ({tuits = [], refreshTuits}) => {
                             <Tuit key={tuit._id}
                                   deleteTuit={deleteTuit}
                                   likeTuit={likeTuit}
+                                  dislikeTuit = {dislikeTuit}
                                   tuit={tuit}/>)
                     }
                 </ul>
--- src/components/tuits/tuit-stats.js
+++ src/components/tuits/tuit-stats.js
@@ -1,7 +1,7 @@
 import React from "react";
 
 
-const TuitStats = ({tuit, likeTuit = () => {}}) => {
+const TuitStats = ({tuit, likeTuit, dislikeTuit = () => {}}) => {
     return (
         <div className="row mt-2">
             <div className="col">
@@ -15,15 +15,28 @@ const TuitStats = ({tuit, likeTuit = () => {}}) => {
             <div className="col">
           <span onClick={() => likeTuit(tuit)}>
               {
-                  tuit.stats && tuit.stats.likes && tuit.stats.likes > 0 &&
-                  <i className="fas fa-heart me-1" style={{color: 'red'}}></i>
+                  tuit.stats  && tuit.stats.likes > 0 &&
+                  <i className="fa-regular fa-thumbs-up me-1" style={{color: 'red'}}></i>
               }
               {
-                  tuit.stats && tuit.stats.likes && tuit.stats.likes <= 0 &&
-                  <i className="far fa-heart me-1"></i>
+                  tuit.stats  && tuit.stats.likes <= 0 &&
+                  <i className="fa-regular fa-thumbs-up me-1"></i>
               }
               {tuit.stats && tuit.stats.likes}
           </span>
+            </div>
+            <div className="col">
+                <span onClick={()=>dislikeTuit(tuit)}>
+                    {
+                        tuit.stats  && tuit.stats.dislikes > 0 &&
+                        <i className="fa-regular fa-thumbs-down me-1" style={{color: 'blue'}}></i>
+                    }
+                    {
+                        tuit.stats  && tuit.stats.dislikes <= 0 &&
+                        <i className="fa-regular fa-thumbs-down me-1"></i>
+                    }
+                    {tuit.stats && tuit.stats.dislikes}
+              </span>
             </div>
             <div className="col">
                 <i className="far fa-inbox-out"></i>
